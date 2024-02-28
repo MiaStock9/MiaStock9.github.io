4ad159ea128e075d1486fcf807e256d7e4f55c57
MiaStock9 <miastock20@gmail.com>
2024-02-28 08:36:58 +0200

    another commit attempt

--- Disa_uniflora_GIS.R
+++ Disa_uniflora_GIS.R
@@ -56,10 +56,6 @@ vegsub %>% group_by(National_) %>%
 install.packages("rinat")
 library(rinat)
 
-#du <- get_inat_obs(taxon_name = "Disa uniflora",
-                   #maxresults = 1000)
-head(du)
-
 #Lesser double collared sunbird
 cc <- get_inat_obs(taxon_name = "Cinnyris chalybeus",
                    maxresults = 1000)
@@ -68,11 +64,6 @@ av <- get_inat_obs(taxon_name = "Anthobaphes violacea",
                    maxresults = 1000)
 
 # Filtering to make sure I only get the research grade observations
-du <- du %>% filter(positional_accuracy<46 &
-                      latitude<0 &
-                      !is.na(latitude) &
-                      captive_cultivated == "false" &
-                      quality_grade == "research")
 cc <- cc %>% filter(positional_accuracy<46 &
                       latitude<0 &
                       !is.na(latitude) &
@@ -84,7 +75,6 @@ av <- av %>% filter(positional_accuracy<46 &
                       captive_cultivated == "false" &
                       quality_grade == "research")
 
-class(du)
 
 #I need to turn this from a data frame into a spatial object
 du <- st_as_sf(du, coords = c("longitude", "latitude"), crs = 4326)
@@ -104,17 +94,18 @@ library(ggspatial)
 library(leaflet)
 library(prettymapr)
 
-#Plot a basic map
+####Plot a basic map####
 ggplot() +
   annotation_map_tile(type = "osm", progress = "none") +
-  geom_sf(data = cc) #this won't work
+  geom_sf(data = cc, color = "#2E8B57") 
 
 ggplot() +
   annotation_map_tile(type = "osm", progress = "none") +
-  geom_sf(data = av)
+  geom_sf(data = av, color = "#FF7F00")
 
-# Plot an interactive map
-leaflet() %>%
+
+#The interactive map
+intmap <-leaflet() %>%
   addTiles(group = "Default") %>%
   addCircleMarkers(data = cc,
                    group = "Cinnyris chalybeus",
@@ -123,18 +114,80 @@ leaflet() %>%
   addCircleMarkers(data = av,
                    group = "Anthobaphes violacea",
                    radius = 1,
-                   color = "orange")
+                   color = "orange") %>%
+  addLegend(position = "bottomright",
+            colors = c("orange","green"),
+            labels = c("Orange breasted","Double collared"))
+intmap
 
 #Getting the remnants layer
 vegr <- st_read("cape_peninsula/cape_peninsula/veg/Vegetation_Indigenous_Remnants.shp")
 hmm <- st_intersection(du, vegr)
 st_crs(du)
+
 #Making the crs the same for both
-av <- st_transform(av, st_crs(vegr))
-cc <- st_transform(cc, st_crs(vegr))
+av2 <- st_transform(av, st_crs(vegr))
+cc2 <- st_transform(cc, st_crs(vegr))
 
 # Another attempt at plotting data
 ggplot() +
   annotation_map_tile(type = "osm", progress = "none") +
-  geom_sf(data=cc, color = "green") +
-  geom_sf(data=av, color = "orange")
+  geom_sf(data=cc, color = "#2E8B57") +
+  geom_sf(data=av, color = "#FF7F00")
+
+#### Attempt at reading in elevation data ####
+install.packages("terra")
+library(terra)
+elevation <-  rast("cape_peninsula/cape_peninsula/CoCT_10m.tif")
+elevation
+# checking the CRS
+crs(elevation)
+st_crs(elevation)
+
+#Crop the elevation so that the size is reduced.
+elevation <- crop(elevation, ext(c(-66642.18, -40412.18, -4019953.29, -3750723.29)))
+
+#Plot the elevation data
+plot(elevation)
+
+# aggregate to reduce file size
+ele30 <- aggregate(elevation, fact = 3, fun = mean)
+
+#Plotting again, but in tidyverse
+ele30 %>% as.data.frame(xy = TRUE)
+  ggplot() +
+  geom_raster(aes(x = x, y = y, fill = `10m_BA`)) # Why is this fat??
+
+#Binding cc and av (from section 8.9) so go back to this section and work out the rest!!
+ccBind <- rbind(cc, av)
+#crop ccBind 
+ccBind <- st_crop(ccBind, ext(c(-66642.18, -40412.18, -4019953.29, -3750723.29)))
+
+ccBind$scientific_name <- str_replace_all(ccBind$scientific_name,
+                                    c("Cinnyris chalybeus chalybeus" = "Cinnyris chalybeus",
+                                      "Cinnyris chalybeus subalaris" = "Cinnyris chalybeus",
+                                      "Cinnyris chalybeus albilateralis" = "Cinnyris chalybeus"))
+
+#Extract the data points
+data <- terra::extract(ele30, vect(ccBind))
+
+#Making a box plot for spp
+ccBind$elevation <- data$`10m_BA`
+ccBind %>% ggplot() +
+  geom_boxplot(aes(scientific_name, elevation), color = c("#FF7F00", "#2E8B57")) 
+
+ #THIS WONT WORK EITHER
+
+
+# Trying to plot many maps on the same thing
+ggplot() +
+  geom_contour(data = as.data.frame(ele30, xy = TRUE),
+              aes(x = x, y = y, z = `10m_BA`), breaks = seq(0, 1100, 100), colour = "black") +
+  geom_sf(data=ccBind, colour = "red")
+  # this doesnt work
+
+ggplot() +
+  annotation_map_tile(type = "osm", progress = "none") +
+  geom_contour(data = as.data.frame(ele30, xy = TRUE),
+               aes(x = x, y = y, z = `10m_BA`), breaks = seq(0, 1100, 100), colour = "black") +
+  geom_sf(data=ccBind, colour = "red", fill = "red")
